57102d4fe98ed3d5b676e024093a509f08cca292
Arturo Rodriguez <arturo.rdz3000@gmail.com>
2023-12-30 12:33:01 +0000

    Fixed issues with the getPostFetcher function

--- url-shortener/src/app/utils/swr-http-utils.ts
+++ url-shortener/src/app/utils/swr-http-utils.ts
@@ -1,5 +1,5 @@
-const SHORTENER_SERVICE_ENDPOINT = 'https://tinyurl.com/';
-const API_TOKEN = process.env.SHORTENER_API_TOKEN;
+const SHORTENER_SERVICE_ENDPOINT = 'https://localhost:3001';
+const API_TOKEN = process.env.NEXT_PUBLIC_SHORTENER_API_TOKEN;
 
 export const doFetch = async (url: string, requestInit: RequestInit | undefined) => {
     return (await fetch(url, requestInit)).json();
@@ -11,11 +11,16 @@ export const getShortenerServiceEndpoint = () => {
 
 export const  getPostFetcher = (body: any) => {
     return async () => {
+        const url = `${getShortenerServiceEndpoint()}create`;
         const request: RequestInit = {
             method: 'POST',
-            body,
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': 'Bearer ' + API_TOKEN, 
+            },
         }
 
-        return await doFetch(`${getShortenerServiceEndpoint()}?api_token=${API_TOKEN}`, request);
+        return await doFetch(url, request);
     }
 }
